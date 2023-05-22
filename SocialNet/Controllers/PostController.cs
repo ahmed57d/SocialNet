9eff8a037ccd5be5ef2a8efe056b6f6f6921b525
Ahmed Hisham <ahmed57d@gmail.com>
2023-05-22 21:26:00 +0300

    adding postController

---
+++ SocialNet/Controllers/PostController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNet.Domain.Models;
+using SocialNet.Repository;
+using SocialNet.Service.IService;
+
+namespace SocialNet.Controllers
+{
+    public class PostController : Controller
+    {
+        private readonly IPostService postService;
+        public PostController(IPostService postService)
+        {
+            this.postService = postService;
+        }
+        [HttpGet]
+        public IEnumerable<Post> GetAllUsersPosts()
+        {
+            var posts = postService.GetAllUsersPosts();
+            return (IEnumerable<Post>)Ok(posts);
+        }
+        [HttpGet("{Id:string}", Name = "Id")]
+        public IEnumerable<Post> GetUserPosts(string Id)
+        {
+            var post = postService.GetUserPosts(Id);
+            return ((IEnumerable<Post>)Ok(post));
+        }
+        [HttpPost]
+        public void AddPost(Post post)
+        {
+            postService.AddPost(post);
+        }
+        [HttpPut]
+        public void UpdatePost(Post post)
+        {
+            postService.UpdatePost(post);
+        }
+        [HttpDelete]
+        public void DeletePost(Post post)
+        {
+            postService.DeletePost(post);
+        }
+    }
+}
